6f29193007e89e34dc440d8fbd87fad3cb2ab4d4
andreapaola2018 <apulloa@miners.utep.edu>
2023-04-28 14:29:51 -0600

    updated queries

--- prototype.py
+++ prototype.py
@@ -11,51 +11,6 @@ onto = get_ontology('data_project.owl').load()
 ##try loading into tkinter window with label 
 onto.load()
 
-##show all directors in the datasets
-directors = onto.search(is_a = onto.Director)
-directorsList= list(directors)
-
-# print(list(default_world.sparql_query("""
-#            PREFIX example: http://www.semanticweb.org/manuelgutierrez/ontologies/2023/3/project
-#            SELECT ?movie 
-#            WHERE {
-#                 ?movie rdf:type example:Movie .
-#                 ?movie example:hasBudget ?budget .
-#             FILTER (?budget <= 200000000)
-  
-#            }
-#     """)))
-
-first_query = list(default_world.sparql("""
-            SELECT ?movie
-            WHERE {
-              ?movie rdf:type data_project:Movie .
-              ?movie data_project:movie_public_score ?public_score .
-              ?movie data_project:movie_critic_score ?people_score .
-              FILTER (?public_score > 70 && ?people_score > 70)
-            }
-        """))
-        
-
-second_query = list(default_world.sparql("""
-            SELECT ?director
-            WHERE {
-                ?director rdf:type data_project:Director .
-            }
-        """))
-        
-third_query = list(default_world.sparql("""
-            SELECT ?movie
-            WHERE {
-                ?movie rdf:type data_project:Movie .
-                ?movie data_project:movie_public_score ?public_score .
-                ?movie data_project:movie_budget ?budget_score .
-                FILTER (?public_score > 75 && ?budget_score > 100000000)
-            }
-        """))
-print(third_query)
-
-
 root=tk.Tk()
  
 # setting the windows size
@@ -64,57 +19,124 @@ root.geometry("600x400")
 # declaring string variable
 # for storing name 
 name_var=tk.StringVar()
-budget_var=tk.StringVar()
- 
-  
-# defining a function that will
-# get the name 
-# print them on the screen
-def submit():
- 
-    name=name_var.get()
-     
-    print("The name is : " + name)
-     
-    name_var.set("")
 
 def onClick():
    
-        tkinter.messagebox.showinfo("Welcome to GFG.",  second_query) 
+        tkinter.messagebox.showinfo("Welcome to GFG.",  print_query_format(second_query())) 
 
 def onClick2():
-        tkinter.messagebox.showinfo("Welcome to GFG.",  first_query) 
+        tkinter.messagebox.showinfo("Welcome to GFG.",  print_query_format(first_query())) 
+        
+def onClick3():
+        tkinter.messagebox.showinfo("Welcome to GFG.",  print_query_format(third_query())) 
+        
+def onClick4():
+        tkinter.messagebox.showinfo("Welcome to GFG.",  print_query_format(fourth_query()))
+        
+        
+
+
+def first_query():
+    first_query = list(default_world.sparql("""
+                SELECT ?movie
+                WHERE {
+                  ?movie rdf:type data_project:Movie .
+                  ?movie data_project:movie_public_score ?public_score .
+                  ?movie data_project:movie_critic_score ?people_score .
+                  FILTER (?public_score > 70 && ?people_score > 70)
+                }
+            """))
+    return first_query
+
+
+def second_query(): 
+    second_query = list(default_world.sparql("""
+                SELECT ?director
+                WHERE {
+                    ?director rdf:type data_project:Director .
+                }
+            """))
+    return second_query
+
+def third_query():
+        
+    third_query = list(default_world.sparql("""
+                SELECT ?movie
+                WHERE {
+                    ?movie rdf:type data_project:Movie .
+                    ?movie data_project:movie_public_score ?public_score .
+                    ?movie data_project:movie_budget ?budget_score .
+                    FILTER (?public_score > 75 && ?budget_score > 100000000)
+                }
+            """))
+    return third_query
+        
+        
+def fourth_query():
+    fourth_query = list(default_world.sparql("""
+                SELECT ?director
+
+                WHERE {
+                    ?director rdf:type data_project:Director .
+
+                    ?movie data_project:movie_critic_score ?critic_score .
+
+                    FILTER (?critic_score > 70)
+                }
+            """))
+    return fourth_query
+    
+    
+def print_query_format(query):
+    
+    format_str = ""
+    for i in range(len(query)):
+        print(query[i][0].name)
+        format_str = format_str + "," + query[i][0].name
+        ##print("this is format name so far", format_str)
+        
+    return format_str
+        
+
+if __name__ == '__main__':
+    
+    print(print_query_format(fourth_query()))
 
         
      
 # creating a label
-name_label = tk.Label(root, text = 'Movie Name', font=('calibre',10, 'bold'))
 # Create a Button
+directorsLabel = tk.Label(root, text = 'All directors', font=('calibre',15, 'bold'))
 button = Button(root, text="Show Directors", command=onClick, height=2, width=8) 
-query1Button = Button(root, text="public score and a critic score of 70 or above", command=onClick2, height=2, width=8) 
 
+query1Label = tk.Label(root, text = 'What movies have a public score and a critic score of 70 or above ?', font=('calibre',15, 'bold'))
+query1Button = Button(root, text="Click here", command=onClick2, height=2, width=10)
+
+query3Label = tk.Label(root, text = 'What movies have a public score over 75 and a budget over 100 M?', font=('calibre',15, 'bold'))
+query3Button = Button(root, text="Click here", command=onClick3, height=2, width=10) 
+
+query4Label = tk.Label(root, text = 'What directors have movies with a critic score 70 or above?', font=('calibre',15, 'bold'))
+query4Button = Button(root, text="Click here", command=onClick4, height=2, width=10) 
+ 
 
   
-# creating a entry for input
-name_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
-    
-# creating a button using the widget
-# Button that will call the submit function
-sub_btn=tk.Button(root,text = 'Submit', command = submit)
-  
-# placing the label and entry in
-# the required position using grid
-# method
-name_label.grid(row=0,column=0)
-name_entry.grid(row=0,column=1)
-sub_btn.grid(row=2,column=1)
+# grid method
+directorsLabel.grid(row=5,column=0)
 button.grid(row=6,column=0)
+query1Label.grid(row=7, column=0)
 query1Button.grid(row=8,column=0)
+query3Label.grid(row=9, column=0)
+query3Button.grid(row=10,column=0)
+query4Label.grid(row=11, column=0)
+query4Button.grid(row=12,column=0)
+
 
   
 # performing an infinite loop
 # for the window to display
 root.mainloop()
+
+
 
 
 
